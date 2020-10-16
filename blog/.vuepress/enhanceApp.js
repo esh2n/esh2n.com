f60b65a18b920a8dc6bf89f5e3768468c7e60861
shunyaendoh1215 <shunyaendoh.bz@gmail.com>
2020-10-16 10:19:28 +0900

    🍉 Add: add Dockerfile

--- blog/.vuepress/enhanceApp.js
+++ blog/.vuepress/enhanceApp.js
@@ -6,5 +6,25 @@ export default ({
   router, // the router instance for the app
   siteData, // site metadata
 }) => {
-  // ...apply enhancements to the app
+//  if (typeof window !== "undefined") {
+//     import("vue-google-adsense")
+//       .then(module => {
+//         const Ads = module.default;
+//         Vue.use(require("vue-script2"));
+//         Vue.use(Ads.Adsense); // ディスプレイ広告
+//         // Vue.use(Ads.InArticleAdsense); // 記事内広告
+//         // Vue.use(Ads.InFeedAdsense); // フィード内広告
+//       })
+//       .catch(e => {
+//         console.log(e);
+//       });
+//   }
+
+//   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+// <script>
+//   (adsbygoogle = window.adsbygoogle || []).push({
+//     google_ad_client: "ca-pub-1469905359837693",
+//     enable_page_level_ads: true
+//   });
+// </script>
 };
